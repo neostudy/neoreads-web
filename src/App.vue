4a00162b3fbab61d89c20c3a1ee02abc2bf0c019
zhaopuming <zhaopuming@gmail.com>
2019-09-23 23:47:12 +0800

    update version

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
                   <span class="theme">{{$t('logo-neo')}}</span>
                   <span class="black">{{$t('logo-reads')}}</span>
                 </router-link>
-                <span id="logo-version">v0.3.3</span>
+                <span id="logo-version">v0.3.4</span>
               </div>
             </el-col>
             <el-col :span="12">
